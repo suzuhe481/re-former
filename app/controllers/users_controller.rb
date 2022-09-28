66b45c0cf105360961968fd76fa9c7e133a1c457
Hector <baseballhector@gmail.com>
2022-09-28 18:04:29 -0400

    Made and implemented #user_params method to more easily get user form data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,20 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+    # @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end
