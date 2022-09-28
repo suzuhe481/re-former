e7454627753eb9d5f8ce5fc9c1f6effacac51867
Hector <baseballhector@gmail.com>
2022-09-28 17:45:21 -0400

    Saving new user with #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
