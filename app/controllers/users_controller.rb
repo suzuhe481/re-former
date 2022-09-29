16ce040a0a02f0fa7229c3cae047fa290a853a28
Hector <baseballhector@gmail.com>
2022-09-28 21:17:45 -0400

    Changed #create to accept user attributes again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    # @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+    @user = User.new(email: params[:email], username: params[:username], password: params[:password])
 
-    @user = User.new(user_params)
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
